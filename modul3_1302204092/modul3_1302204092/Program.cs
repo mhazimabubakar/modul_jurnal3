e159ec902ac3829663f84db970924c9116dd34b5
unknown <hazimbitw@gmail.com>
2022-03-12 09:57:56 +0700

    menambahkan class KodeBuah

--- modul3_1302204092/modul3_1302204092/Program.cs
+++ modul3_1302204092/modul3_1302204092/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace modul3_1302204092
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
